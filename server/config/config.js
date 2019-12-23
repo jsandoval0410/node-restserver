c6b9b483cc3f9f812d4caeb5f594ee8802f82614
Jhan Sandoval <jsandoval@smartsoftint.com>
2019-12-22 22:19:06 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jsandoval:6NbJrWhLzQNqdbyX@cluster0-xg4d6.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.urlDB = urlDB;
